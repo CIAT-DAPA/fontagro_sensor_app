d328fa3e68cc9d32ad428cd358d19d7b55ccf2ca
carlosn97 <carlosnasayov@gmail.com>
2022-10-07 12:35:30 -0500

    basename agregado para despliegue

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return ( 
     <div className="App">
       {/* <ShowPlots /> */}
-      <Routes>
+      <Routes basename='/fontagro_sensor_app'>
       
       <Route path='/' element={<Home  />}></Route>
       <Route path='/data' element={<><Header /> <LoadData /> </>}></Route>
