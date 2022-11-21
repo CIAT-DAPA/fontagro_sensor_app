9f25316415c71edd5ad7ee2ced3acf1e8aadef8d
carlosn97 <carlosnasayov@gmail.com>
2022-11-20 23:47:57 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,40 @@
+let CACHE_NAME= 'codepwa'
+var urlCache= [
+    '/',
+    'https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
+   'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js',
+    'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+   'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
+   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js',
+    '/static/js/bundle.js',
+    '/manifest.json',
+    '/static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
+    '/logo192.png'
+    
+]
+this.addEventListener('install',(e)=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache)=>{
+            return cache.addAll(urlCache)
+        })
+    )
+})
+//fetch cahe data
+this.addEventListener('fetch',(e)=>{
+    if(!navigator.onLine){
+        console.log('fuiste offline')
+        e.respondWith(
+            caches.match(e.request)
+            .then((response)=>{
+                if(response){
+                    return response
+                }
+                let fUrl= e.request.clone()
+                fetch(fUrl)
+            })
+           
+        )
+    }
+})
