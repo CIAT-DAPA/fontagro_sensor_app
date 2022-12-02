63c698e416dba4fb8d7cf2bb893112872a87dd79
carlosn97 <carlosnasayov@gmail.com>
2022-12-02 09:37:08 -0500

    Updates

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ this.addEventListener('install',(e)=>{
     e.waitUntil(
         caches.open(CACHE_NAME)
         .then((cache)=>{
-            cache.addAll(urlCache)
+           return cache.addAll(urlCache)
         })
     )
 })
