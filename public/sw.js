9c2365e483c7b12b5d641e68ccfda5c4252693eb
carlosn97 <carlosnasayov@gmail.com>
2022-12-02 17:39:26 -0500

    rutas sin stati

--- public/sw.js
+++ public/sw.js
@@ -9,8 +9,8 @@ var urlCache= [
    'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js',
     /* //'https://tufincastats.open-aeps.org/static/js/bundle.js',
-    'https://tufincastats.open-aeps.org/static/manifest.json',
-    'https://tufincastats.open-aeps.org/static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
+    /* 'https://tufincastats.open-aeps.org/static/manifest.json' */,
+    'static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
     'https://tufincastats.open-aeps.org/static/logo192.png',
     'https://tufincastats.open-aeps.org/static/media/logo-ciat.9ff4996f25c3c24535b4.png',
     'https://tufincastats.open-aeps.org/static/media/logo-visualiti.c86e53ae15dea7aab9cf.png',
@@ -19,7 +19,7 @@ var urlCache= [
     'https://tufincastats.open-aeps.org/static/media/nica.7c6eb28b77a68ad354e0.png',
     'https://tufincastats.open-aeps.org/static/media/honduras.babd8cb8d2770b8994de.png',
     'https://tufincastats.open-aeps.org/static/media/ubicación.05c360e939642f59feba.png',
-    'https://tufincastats.open-aeps.org/static/media/sensor.dc6d14c071e6de1f165a.jpeg', */
+    'https://tufincastats.open-aeps.org/static/media/sensor.dc6d14c071e6de1f165a.jpeg',
 
 
     //
