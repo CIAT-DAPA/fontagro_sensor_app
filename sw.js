55cd0aec9eb5b665c4ecea2ace92b8477fcdba46
carlosn97 <carlosnasayov@gmail.com>
2022-12-02 17:28:11 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 let CACHE_NAME= 'codepwa'
 var urlCache= [
+    
     'https://tufincastats.open-aeps.org/',
     'https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
@@ -7,7 +8,7 @@ var urlCache= [
     'https://code.jquery.com/jquery-3.2.1.slim.min.js',
    'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js',
-    //'https://tufincastats.open-aeps.org/static/js/bundle.js',
+    /* //'https://tufincastats.open-aeps.org/static/js/bundle.js',
     'https://tufincastats.open-aeps.org/static/manifest.json',
     'https://tufincastats.open-aeps.org/static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
     'https://tufincastats.open-aeps.org/static/logo192.png',
@@ -18,7 +19,7 @@ var urlCache= [
     'https://tufincastats.open-aeps.org/static/media/nica.7c6eb28b77a68ad354e0.png',
     'https://tufincastats.open-aeps.org/static/media/honduras.babd8cb8d2770b8994de.png',
     'https://tufincastats.open-aeps.org/static/media/ubicación.05c360e939642f59feba.png',
-    'https://tufincastats.open-aeps.org/static/media/sensor.dc6d14c071e6de1f165a.jpeg',
+    'https://tufincastats.open-aeps.org/static/media/sensor.dc6d14c071e6de1f165a.jpeg', */
 
 
     //
