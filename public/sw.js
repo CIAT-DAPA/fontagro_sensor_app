244c59cd7de21c4631beb31020f291af06210966
carlosn97 <carlosnasayov@gmail.com>
2022-12-05 15:51:00 -0500

    main updated

--- public/sw.js
+++ public/sw.js
@@ -10,12 +10,13 @@ var urlCache= [
    'https://tufincastats.open-aeps.org/static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
    'https://tufincastats.open-aeps.org/static/css/main.20b7941d.css',
-   'https://tufincastats.open-aeps.org/static/js/main.da0162cf.js',
+   'https://tufincastats.open-aeps.org/static/js/main.601a2edc.js',
    'https://tufincastats.open-aeps.org/static/media/ubicaci%C3%B3n.05c360e939642f59feba.png',
    'https://tufincastats.open-aeps.org/static/media/colombia.0a557c5eba997e0e7a74.png',
    'https://tufincastats.open-aeps.org/static/media/nica.7c6eb28b77a68ad354e0.png',
    'https://tufincastats.open-aeps.org/static/media/sensor.dc6d14c071e6de1f165a.jpeg',
    'https://tufincastats.open-aeps.org/static/media/honduras.babd8cb8d2770b8994de.png'
+   
 ]
 this.addEventListener('install',(e)=>{
     e.waitUntil(
