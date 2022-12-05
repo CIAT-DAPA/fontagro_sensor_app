2b481078bf801480dfa2bb76d3f2af8d99e79ed0
carlosn97 <carlosnasayov@gmail.com>
2022-12-05 13:31:46 -0500

    agregads rutass

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,15 @@
 let CACHE_NAME= 'codepwa'
 var urlCache= [
-    'https://tufincastats.open-aeps.org',
+    '/',
     'https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
    'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js',
     'https://code.jquery.com/jquery-3.2.1.slim.min.js',
    'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js',
-   'https://tufincastats.open-aeps.org/static/media/background-data-user.dba68cb1ca080ea9860a.jpg'
+   'https://tufincastats.open-aeps.org/static/media/background-data-user.dba68cb1ca080ea9860a.jpg',
+   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+   'https://tufincastats.open-aeps.org/static/css/main.20b7941d.css'
 
 ]
 this.addEventListener('install',(e)=>{
