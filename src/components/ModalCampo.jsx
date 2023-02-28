fed8669577dfd5bd79214607047db39c31a18f7a
carlosn97 <carlosnasayov@gmail.com>
2023-02-28 10:44:40 -0500

    campos agregados

--- src/components/ModalCampo.jsx
+++ src/components/ModalCampo.jsx
@@ -29,7 +29,12 @@ function ModalCampo({showc, handleClosec}){
             <option className="suelo"  value={'9,4,Arenoso'}>Arenoso (A)</option>
             <option className="suelo" value={'14,6,Franco Arenoso'}>Franco Arenoso (FA)</option>
             <option className="suelo" value={'22,15,Franco'}>Franco (F)</option>
+            <option className="suelo" value={'30,15,Franco Limoso'}>Franco Limoso (FL)</option>
+            <option className="suelo" value={'30,6,Limoso'}>Limoso (L)</option>
+            <option className="suelo" value={'40,20,Arcilloso Limoso'}>Arcilloso Limoso (ArL)</option>
+            <option className="suelo" value={'21,9,Arenoso Franco'}>Arenoso Franco (AF)</option>
             <option className="suelo" value={'27,13,Franco Arcilloso'}>Franco Arcilloso (FAr)</option>
+            <option className="suelo" value={'28,15,Franco Arcilloso Limoso'}>Franco Arcilloso Limoso (FArL)</option>
             <option className="suelo" value={'24,12,Franco Arcillo Arenoso'}>Franco Arcillo Arenoso (FArA)</option>
             <option className="suelo" value={'31,15,Arcillo Arenoso'}>Arcillo Arenoso (ArA)</option>
             <option className="suelo" value={'35,17,Arcilloso'}>Arcilloso (Ar)</option>
